ea282bb1f05bd1b3821527087953a602ea220738
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo-2.local>
2020-08-24 17:08:10 -0500

    first commit

---
+++ src/main/java/co/edu/eam/disenosoftware/homeautobackend/HomeautoBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.eam.disenosoftware.homeautobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeautoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeautoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/eam/disenosoftware/homeautobackend/HomeautoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eam.disenosoftware.homeautobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeautoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
