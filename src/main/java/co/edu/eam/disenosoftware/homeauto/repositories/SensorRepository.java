258511168c5d40139a64ec86b6e332fce5030740
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo-2.local>
2020-09-18 17:21:53 -0500

    ¨services

--- src/main/java/co/edu/eam/disenosoftware/homeauto/repositories/SensorRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/homeauto/repositories/SensorRepository.java
@@ -1,10 +1,8 @@
 package co.edu.eam.disenosoftware.homeauto.repositories;
 
-import co.edu.eam.disenosoftware.homeauto.model.entities.Room;
 import co.edu.eam.disenosoftware.homeauto.model.entities.Sensor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -18,11 +16,9 @@ public class SensorRepository {
   @PersistenceContext
   private EntityManager em;
 
-
   public void create(Sensor sensor) {
     em.persist(sensor);
   }
-
 
   public Sensor find(Long id) {
     return em.find(Sensor.class, id);
